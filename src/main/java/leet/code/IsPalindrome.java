69b21c51c0bfb3d001d5b49ca59f054bc7f83a41
jose alberto cruz martinez <albertocruz8133@proton.me>
2026-01-07 19:56:08 -0600

    refactor: Reemplaza el uso de String por StringBuilder

--- src/main/java/leet/code/IsPalindrome.java
+++ src/main/java/leet/code/IsPalindrome.java
@@ -1,27 +1,27 @@
 package leet.code;
 
-import java.util.Arrays;
-
 public class IsPalindrome {
 
     // Given an integer x, return true if x is a palindrome, and false otherwise.
 
     /**
-     * Refactoriza la solución anterior agregando como restricción no usar métodos nativos
-     * para invertir la cadena, esta forma de resolver el problema implica convertir el parámetro
-     * en una cadena y de ahi obtenerlo como un arreglo de caracteres y recorrerlo desde el final al inicio
-     * pero aumenta el tiempo de runtime de 8ms con StringBuilder a 10ms con la forma actual
+     * Anteriormente, se agrega como restricción el uso de métodos nativos para invertir cadenas
+     * Entonces se usó un String para concatenar cada carácter del parámetro, pero esa solución
+     * agregaba 3ms más de runtime según el medidor de leetcode, pero si se cambia el String por
+     * StringBuilder el tiempo de runtime vuelve a 8ms esto se debe a que al usar un
+     * String y concatenarle no estás usando el mismo espacio en memoria si no creas uno nuevo
+     * por cada concatenación a diferencia de StringBuilder que no crea nuevos espacios en memoria
      */
     public static boolean isPalindrome(int x) {
         String originally = String.valueOf(x);
-        String reverse = "";
+        StringBuilder reverse = new StringBuilder();
         char[] characters = originally.toCharArray();
 
         for (int i = originally.length() - 1; i >= 0; i--) {
-            reverse += characters[i];
+            reverse.append(characters[i]);
         }
 
-        return reverse.equals(originally);
+        return reverse.toString().equals(originally);
     }
 
     public static void main(String[] args) {
